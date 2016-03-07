3e22d0124fc88c5468c939c1b719bc24c0ff480a
Goden <goden.incredible@gmail.com>
2016-03-08 00:56:50 +0800

    Create a new controller and attach the controller to page.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
 (function() {
 	var app = angular.module("store", []);
+	app.controller("StoreController", function() {
+		this.product = gem;
+	});
+
+
+	// prepare data for presentation
+	// 
+	var gem  = {
+		name: "尊爵紫寶石",
+		price: 200000,
+		description: "純正的愛情 - 紫水晶代表靈性、精神、高層次的愛意，可作對仰慕者的一種定情物、信物。"
+	};
+
 })();
