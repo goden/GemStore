f8abe9ee145f1f910e23f39840a31b6c48447e5e
Goden <goden.incredible@gmail.com>
2016-03-08 01:33:08 +0800

    Arrange the products into an array.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 (function() {
 	var app = angular.module("store", []);
 	app.controller("StoreController", function() {
-		this.product = gem;
+		this.products = gems;
 	});
 
 
 	// prepare data for presentation
 	// 
-	var gem  = [{
+	var gems  = [{
 		name: "尊爵紫寶石",
 		price: 200000,
 		description: "純正的愛情 - 紫水晶代表靈性、精神、高層次的愛意，可作對仰慕者的一種定情物、信物。",
